94ce0cbfe2b9423f7cffa8bf06bef44d26cc9ae5
kikisu0226 <kikisu0226@gmail.com>
2026-09-14 14:52:36 +0800

    Update weather_alert.py

--- weather_alert.py
+++ weather_alert.py
@@ -1,4 +1,33 @@
 import os
 import requests
 
-print("Python 測試成功")
+LATITUDE = 25.0330
+LONGITUDE = 121.5654
+
+THRESHOLD = 30
+
+def get_weather():
+url = "https://api.open-meteo.com/v1/forecast"
+
+```
+params = {
+    "latitude": LATITUDE,
+    "longitude": LONGITUDE,
+    "daily": "precipitation_probability_max,precipitation_sum",
+    "timezone": "Asia/Taipei",
+    "forecast_days": 1
+}
+
+response = requests.get(url, params=params, timeout=10)
+response.raise_for_status()
+
+data = response.json()
+
+date = data["daily"]["time"][0]
+
+rain_probability = data["daily"][
+    "precipitation_probability_max"
+][0]
+
+rain
+```
